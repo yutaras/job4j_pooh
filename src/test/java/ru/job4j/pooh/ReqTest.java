834ff22b7c8a3a4dc547be6a19f20849780b9a68
Yuriy Tarasenko <maxjeff@mail.ru>
2022-07-10 23:56:01 +0300

    job4_pooh

--- src/test/java/ru/job4j/pooh/ReqTest.java
+++ src/test/java/ru/job4j/pooh/ReqTest.java
@@ -10,14 +10,14 @@ public class ReqTest {
     @Test
     public void whenQueueModePostMethod() {
         String ls = System.lineSeparator();
-        String content = "POST /queue/weather HTTP/1.1" + ls +
-                "Host: localhost:9000" + ls +
-                "User-Agent: curl/7.72.0" + ls +
-                "Accept: */*" + ls +
-                "Content-Length: 14" + ls +
-                "Content-Type: application/x-www-form-urlencoded" + ls +
-                "" + ls +
-                "temperature=18" + ls;
+        String content = "POST /queue/weather HTTP/1.1" + ls
+                + "Host: localhost:9000" + ls
+                + "User-Agent: curl/7.72.0" + ls
+                + "Accept: */*" + ls
+                + "Content-Length: 14" + ls
+                + "Content-Type: application/x-www-form-urlencoded" + ls
+                + "" + ls
+                + "temperature=18" + ls;
         Req req = Req.of(content);
         assertThat(req.httpRequestType(), is("POST"));
         assertThat(req.getPoohMode(), is("queue"));
@@ -42,13 +42,13 @@ public class ReqTest {
     @Test
     public void whenTopicModePostMethod() {
         String ls = System.lineSeparator();
-        String content = "POST /topic/weather HTTP/1.1" + ls +
-                "Host: localhost:9000" + ls +
-                "User-Agent: curl/7.72.0" + ls +
-                "Accept: */*" + ls +
-                "Content-Length: 14" + ls +
-                "Content-Type: application/x-www-form-urlencoded" + ls +
-                "" + ls +
+        String content = "POST /topic/weather HTTP/1.1" + ls
+                + "Host: localhost:9000"
+                + ls + "User-Agent: curl/7.72.0" + ls
+                + "Accept: */*" + ls
+                + "Content-Length: 14" + ls
+                + "Content-Type: application/x-www-form-urlencoded" + ls
+                + "" + ls +
                 "temperature=18" + ls;
         Req req = Req.of(content);
         assertThat(req.httpRequestType(), is("POST"));
