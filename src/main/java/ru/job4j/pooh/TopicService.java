1c3acf61bbadad3d13db2baded0dcbe6e6ae6300
Yuriy Tarasenko <maxjeff@mail.ru>
2022-07-16 16:31:01 +0300

    job4_pooh

---
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -0,0 +1,10 @@
+package ru.job4j.pooh;
+
+import java.sql.SQLOutput;
+
+public class TopicService implements Service {
+    @Override
+    public Resp process(Req req) {
+        return null;
+    }
+}
